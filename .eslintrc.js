87c7a754d4ac4c3c8a874079132a85e55b884c0d
John Iwasz <john@whetstonetechnologies.io>
2020-02-12 22:05:52 -0500

    Migrated from tslint.json to eslint

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "project": "tsconfig.json",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "@typescript-eslint/indent": [
+            "error",
+            "tabs"
+        ],
+        "@typescript-eslint/member-delimiter-style": [
+            "error",
+            {
+                "multiline": {
+                    "delimiter": "semi",
+                    "requireLast": true
+                },
+                "singleline": {
+                    "delimiter": "semi",
+                    "requireLast": false
+                }
+            }
+        ],
+        "@typescript-eslint/semi": [
+            "error",
+            "always"
+        ]
+    }
+};
