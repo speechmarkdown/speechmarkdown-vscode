a8f378235690030a0ccea38302761ad13ff56f94
John Iwasz <iwaszj@gmail.com>
2022-12-23 09:33:45 -0500

    Removed clutter

--- out/pollyAudioPlayer.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-import { PollyClient } from "@aws-sdk/client-polly";
-import { Readable } from "stream";
-export declare class PollyAudioPlayer {
-    client: PollyClient;
-    constructor(region: string, accessKeyId: string, secretAccessKey: string);
-    getAudioStream(ssmlText: string, pollyVoice: string): Promise<Readable>;
-    playAudioStream(ssmlText: string, pollyVoice: string): Promise<void>;
-}
--- out/smdOutputProvider.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class SMLTextWriter {
-    static displaySSMLText(smdText: string): void;
-    static GetSSML(smdText: string, platform: string | null, assistantLabel?: string): string;
-}
