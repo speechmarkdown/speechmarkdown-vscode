99feba50dfc0f1c543f9e11e72d0c8cb96ec8459
John Iwasz <iwaszj@gmail.com>
2022-12-23 09:34:56 -0500

    Removed clutter (#26)
    * Updated to SSML 2.1
    
    * Minor change log and license updates.
    
    * AWS updates to package-lock
    
    * added recommended query to codeql
    
    * Removed clutter
    
    Co-authored-by: John Iwasz <john.iwasz@aveva.com>

--- out/hoverProvider.d.ts
+++
@@ -1,5 +0,0 @@
-import * as vscode from "vscode";
-export declare class JSHoverProvider implements vscode.HoverProvider, vscode.CompletionItemProvider {
-    provideHover(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.Hover>;
-    provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext): vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList>;
-}
--- out/ssmlAudioPlayer.d.ts
+++
@@ -1,5 +0,0 @@
-import { Engine } from "@aws-sdk/client-polly";
-export declare class SSMLAudioPlayer {
-    static getSSMLSpeechAsync(smdText: string, engineType: Engine): Promise<void>;
-    private static playAudio;
-}
